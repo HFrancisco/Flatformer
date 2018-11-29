8aa85b70e4c3047e963edf990f257cfeeb821651
Harrygato <harold_francisco@dlsu.edu.ph>
2018-11-29 19:41:31 +0800

    Final

---
+++ Assets/_Scripts/PhysicsHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsHandler : MonoBehaviour {
+
+	private const float Y_THRESHOLD = 0.0f;
+
+	[SerializeField] private GameObject fallableObjects;
+	[SerializeField] private GOPool playerPool;
+	[SerializeField] private int spawnSize = 5;
+
+	private Vector3 originPositions;
+
+	private const float TIME_DELAY = 0.25f;
+	private float ticks = 0.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.originPositions = new Vector3();
+		this.StoreOriginPositions();
+
+		this.playerPool.Initialize();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if(this.fallableObjects.transform.position.y <= Y_THRESHOLD)
+		{
+			this.ResetToOrigin();
+		}
+
+		//spawn N objects periodically
+		this.ticks += Time.deltaTime;
+		if (this.ticks >= TIME_DELAY)
+		{
+			this.ticks = 0.0f;
+
+			//do the spawning
+			PlayerPool poolableObject = this.playerPool.RequestPoolableBatch(this.spawnSize);
+			if (poolableObject == null)
+			{
+				return;
+			}
+		}
+	}
+
+	private void StoreOriginPositions()
+	{
+		originPositions = this.fallableObjects.transform.position;
+		Debug.Log ("OP is " + originPositions);
+	}
+
+	private void ResetToOrigin()
+	{
+		this.fallableObjects.transform.localPosition = originPositions;
+		//this.fallableObjects[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+	}
+}
---
+++ Assets/_Scripts/PlayerPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPool : APoolable {
+
+	private const float Y_THRESHOLD = -0.7f;
+	private Transform trans;
+
+	// Use this for initialization
+	void Start () {
+		trans = this.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.localPosition.y <= Y_THRESHOLD && this.poolRef != null)
+		{
+			this.poolRef.ReleasePoolable(this);
+		}
+	}
+
+	public override void Initialize()
+	{
+		Debug.Log("Shape Init");
+	}
+
+	public override void OnActivate()
+	{
+
+	}
+
+	public override void Release()
+	{
+		this.transform.position = new Vector3(-0.889f, 0.229f, 0.0f);
+		this.transform.rotation = trans.rotation;
+		this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+	}
+}
